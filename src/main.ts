090bcbbfbdbf75a7b3a2f6f1dd203fe7561c855a
emmanuelfalola <emmanuelfalola9998u@gmail.com>
2024-03-28 13:39:11 +0000

    started adding the first functionality where we are targetting the button container

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import './style.css'
 
 const display = document.querySelector('.iphone-calculator__display-input') as HTMLInputElement;
+const buttonsContainer = document.querySelector('.iphone-calculator__buttons') as HTMLButtonElement;
 
+if (!display || buttonsContainer) throw new Error('something wrong with selectors')
 
-const handleDisplay = (e: Event) => {
-  
+const handleDisplay = () => {
+  console.log(display.value) // logs the value of the input tag; r
 }
 
+const handleButtonSelection = (e:Event) => {
+  const buttonText = e.target as HTMLButtonElement
+  console.log(buttonText.innerText)
+  return buttonText.innerText
+}
+
+
+numberButtons.addEventListener('click', handleButtonSelection)
